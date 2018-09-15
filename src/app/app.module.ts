373f45ceb1d1fd37c08326fdb1a5af0bcd7008b6
Rafal <rmf@europe.com>
2018-09-15 07:24:18 +0200

    search component activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { MusicService } from './music.service';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
